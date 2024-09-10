d04050f1911bbacd5dc0a12693e3dac0588b9c3d
Braiding <140623yy@163.com>
2024-09-11 02:52:49 +0800

    Change the version number to 5.4

--- src/Lua.Core/BinChunk/BinaryChunkConstants.cs
+++ src/Lua.Core/BinChunk/BinaryChunkConstants.cs
@@ -3,7 +3,7 @@ namespace Lua.Core.BinChunk;
 public class BinaryChunkConstants
 {
     public static readonly byte[] LuaSignature = { 0x1B, 0x4C, 0x75, 0x61 };
-    public const byte LuacVersion = 0x53;
+    public const byte LuacVersion = 0x54;
     public const byte LuacFormat = 0;
     public static readonly byte[] LuacData = { 0x19, 0x93, 0x0D, 0x0A, 0x1A, 0x0A };
     public const uint InstructionSize = 4;
